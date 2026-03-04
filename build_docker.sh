6fc12d9fb0e1c3862a6912a490b25d1d1f288f7d
brojoon-10 <shineejoon@naver.com>
2026-03-04 17:03:35 +0900

    small debug

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-INPUT_GID=$(getent group input | cut -d: -f3 2>/dev/null || echo 995)
+INPUT_GID=$(getent group input | cut -d: -f3)
 
 mkdir -p ~/creating_autonomous_car_ws/src/cache/build \
          ~/creating_autonomous_car_ws/src/cache/install \
